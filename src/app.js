b9038130e1978b576aea684bd67b1dc1fa175278
Teodor-Mihai Cosma <79532427+teodorcosma@users.noreply.github.com>
2023-06-20 17:42:44 +0300

    small improvements
    tiny design improvements to make it more similar to requirement

--- src/app.js
+++ src/app.js
@@ -5,16 +5,16 @@ function generateBlogCard(data) {
     return `
     <div class="col-4">
       <div class="p-card">
-        <h5 class="category" style="text-transform: uppercase;">${data._embedded['wp:term'][1][0].name}</h5>
+        <h5 class="category" style="text-transform: uppercase;"><a href=${data._embedded['wp:term'][1][0].link} >${data._embedded['wp:term'][1][0].name} </a> </h5>
         <hr class="u-sv1">
         <div class="p-card__content">
           <img class="p-card__image" alt="" height="185" width="330" src="${data.featured_media}">
           <h3>
             <a href="${data.link}" class="title">${data.title.rendered}</a>
           </h3>
-          <p class="u-no-padding--bottom">By <a class="author">${data._embedded.author[0].name}</a> on <em class="date">${new Date(data.date).toLocaleDateString()}</em></p>
+          <p class="u-no-padding--bottom">By <a href= ${data._embedded.author[0].link} >${data._embedded.author[0].name}</a> on <em class="date">${new Date(data.date).toLocaleDateString()}</em></p>
           <hr class="u-sv1">
-          <h5 style="font-weight: 300;">${data._embedded['wp:term'][0][0].name}</h5>
+          <h5 "><a href=${data._embedded['wp:term'][0][0].link} >${data._embedded['wp:term'][0][0].name} </a> </h5>
         </div>
       </div>
     </div>
